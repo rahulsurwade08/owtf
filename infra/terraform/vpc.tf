34620cd4742b646116bebb92696258ac80b2a1da
RahulSurwade08 <rtr.rahulsurwade@gmail.com>
2024-08-22 19:26:10 -0400

    remove nat instance route table

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -90,12 +90,3 @@ resource "aws_route_table_association" "private_rta" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
 }
-
-# Update the route table for the private subnet to use the NAT Instance
-#resource "aws_route" "private_nat_route" {
-#  route_table_id         = aws_route_table.private_rt.id
-#  destination_cidr_block = var.cidr_block
-#  network_interface_id   = aws_instance.nat_instance.primary_network_interface_id
-#
-#  depends_on = [aws_instance.nat_instance]
-#}
